ce95a1f809adb7abfc42b8e4421b1c9c77a17621
wangyu1202 <wangyu1202@jd.com>
2020-04-27 17:32:51 +0800

    简单的信号量实现

--- src/main/java/com/jd/wangyu/限流/信号量/信号量.java
+++ src/main/java/com/jd/wangyu/限流/信号量/信号量.java
@@ -1,4 +1,20 @@
 package com.jd.wangyu.限流.信号量;
 
+import java.util.concurrent.Semaphore;
+
 public class 信号量 {
+
+    Semaphore semaphore = new Semaphore(5);
+
+    public String acquire() {
+        try {
+            semaphore.acquire(1);
+            return "successed";
+        } catch (InterruptedException e) {
+            return "fail";
+        }finally {
+            semaphore.release(1);
+        }
+    }
 }
+
